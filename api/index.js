6be46a19a374832558353fd1d6aab098bd64ab0b
Brian Nkhata <bnkhata@mra.mw>
2023-12-22 21:29:11 +0200

    created test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL)
@@ -12,10 +14,10 @@ mongoose
     console.log(err);
   });
 
-
-  
 const app = new express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRouter);
