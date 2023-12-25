f1e9263b7c6a8893ad76429a82be93f1374218f6
Brian Nkhata <bnkhata@mra.mw>
2023-12-25 13:49:22 +0200

    created update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,34 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
-  res.send("Hellow Wrold");
+  res.send("Test Route");
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req, body.password, 10);
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      req.body.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...others } = updatedUser._doc;
+  } catch (error) {
+    next(error);
+  }
 };
